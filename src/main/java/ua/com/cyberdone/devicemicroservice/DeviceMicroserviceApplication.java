50698af3498afb73a8a0486c39773b447a59ce90
Ostap Komplikevych <ostap.ja@gmail.com>
2022-02-02 16:07:01 +0200

    [cdds-03] refactor

--- src/main/java/ua/com/cyberdone/devicemicroservice/DeviceMicroserviceApplication.java
+++ src/main/java/ua/com/cyberdone/devicemicroservice/DeviceMicroserviceApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
 import ua.com.cyberdone.devicemicroservice.service.MqttService;
@@ -12,6 +13,7 @@ import ua.com.cyberdone.devicemicroservice.service.MqttService;
 @Slf4j
 @Service
 @EnableScheduling
+@EnableEurekaClient
 @SpringBootApplication
 @RequiredArgsConstructor
 public class DeviceMicroserviceApplication implements CommandLineRunner {
@@ -22,7 +24,7 @@ public class DeviceMicroserviceApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         mqttService.start();
     }
 }
